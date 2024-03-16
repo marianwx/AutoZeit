ccfa0873041d2c272e15c84cb81614593b9eb5c7
maran <maransmk69@gmail.com>
2024-03-16 11:04:44 +0200

    .

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,5 +1,19 @@
 <template>
 
-    
+<div class="sidebar-wrapper-docs">
+   <side-bar title="Sidebar" short-title="S" background-color="vue">
+        <template slot-scope="props" slot="links">
+          <sidebar-item
+                  :link="{
+                    name: 'Dashboard',
+                    icon: 'tim-icons icon-chart-pie-36',
+                    path: '#',
+                    isRoute: false,
+                  }">
 
-</template>
+          </sidebar-item>
+        </template>
+  </side-bar>        
+</div>
+
+</template>>
